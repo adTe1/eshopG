8f2aa0d6763ca460c3c42eaab370154f3b583157
adTe1 <001adte@gmail.com>
2025-01-09 00:02:51 +0200

    Add files via upload

--- backend/routes/prodRoutes.js
+++ backend/routes/prodRoutes.js
@@ -2,21 +2,41 @@
 const express = require('express');
 const prodController = require('../controllers/prodController');
 //const {getAllTours...} = require('./../controllers/tourController');
+const authController =require('./../controllers/authController');
+//const reviewController = require('./../controllers/reviewController');
+const reviewRouter = require('./../routes/reviewRoutes');
 
 const router = express.Router(); //middleware
 
-//router.param('id', prodController.checkID);
+
+router.use('/:productId/reviews', reviewRouter);
+
+router.route('/product-stats').get(prodController.getProductStats);
+// router.route('/monthly-plan/:year').get(prodController.getMonthlyPlan);
 
 router
     .route('/')
     .get(prodController.getAllProducts)
-    .post(prodController.createProduct);
+    .post(
+        authController.protect, 
+        authController.restrictTo('admin', 'moderator'),
+        prodController.createProduct
+    );
 
 router
     .route('/:id')
     .get(prodController.getProduct)
-    .patch(prodController.updateProduct)
-    .delete(prodController.deleteProduct);
+    .patch(
+        authController.protect,
+        authController.restrictTo('admin', 'moderator'),
+        prodController.updateProduct,
+    )
+    .delete(
+        authController.protect,
+        authController.restrictTo('admin', 'moderator'),
+        prodController.deleteProduct
+      );
+
 
     
 module.exports = router;    
